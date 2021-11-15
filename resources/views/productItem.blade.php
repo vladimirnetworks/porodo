9cd065951a09a93792a5602238a1db71e08f1961
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-15 12:20:10 +0330

    improve views

--- resources/views/productItem.blade.php
+++ resources/views/productItem.blade.php
@@ -1,4 +1,4 @@
-<div class="border border-dark col-sm-6 col-lg-3 p-2 text-center border rounded p-2 m-2">
+<div class="border border-dark col-sm-6 col-lg-3 p-2 text-center border rounded">
 
 <a href="{{$product->path}}" class="d-block"><img class="mw-100" src="{{$product->thumb}}"></a>
          
