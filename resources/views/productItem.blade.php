4672c190e7194e38526861a12a45eb7f9cc25727
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-15 12:29:13 +0330

    improve views

--- resources/views/productItem.blade.php
+++ resources/views/productItem.blade.php
@@ -1,6 +1,6 @@
 <div class="col-sm-6 col-lg-3 p-2 text-center border rounded">
 
-<div class="border border-dark">
+<div class="border border-dark h-100">
 <a href="{{$product->path}}" class="d-block"><img class="mw-100" src="{{$product->thumb}}"></a>
          
          <a href="{{$product->path}}" class="d-block">{{$product->title}}</a>
