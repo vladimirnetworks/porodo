995fedfa5ec47de7958f209afdc11fccbe6bfb58
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-15 12:32:06 +0330

    improve views

--- resources/views/productItem.blade.php
+++ resources/views/productItem.blade.php
@@ -1,12 +1,12 @@
 <div class="col-sm-6 col-lg-3 p-2 text-center ">
 
-<div class="border border-secondary h-100 rounded">
+<div class="border border-secondary h-100">
 <a href="{{$product->path}}" class="d-block"><img class="mw-100" src="{{$product->thumb}}"></a>
          
          <a href="{{$product->path}}" class="d-block">{{$product->title}}</a>
 
 
-         {{$product->price}}
+         {{number_format($product->price)}} تومان
 
 </div>
 
